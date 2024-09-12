4c353cfe95af76a64648accd738e12841c7f7c4f
wael elsebaiy <wsebaiy@gmail.com>
2024-09-12 21:09:39 +0200

    initial files upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let score1= document.getElementById("score1");
+let score2 = document.getElementById("score2")
+let score_1 = 0;
+let score_2 = 0;
+function add1(){
+    score_1 += 1
+    score1.textContent =score_1 ;
+}
+
+function add2(){
+    score_1 += 2;
+    score1.textContent =score_1 ;
+}
+
+function add3(){
+    score_1 += 3;
+    score1.textContent =score_1 ;
+}
+
+function add11(){
+    score_2 += 1;
+    score2.textContent =score_2 ;
+}
+
+function add22(){
+    score_2 += 2;
+    score2.textContent =score_2 ;
+}
+
+function add33(){
+    score_2 += 3;
+    score2.textContent =score_2 ;
+}
